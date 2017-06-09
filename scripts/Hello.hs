72028f8f4daf8e367e9ce7906ff0450aebcc643d
Jason Shipman <jasonpshipman@gmail.com>
2017-06-09 14:26:54 +0200

    Add Hello script

---
+++ scripts/Hello.hs
@@ -0,0 +1,10 @@
+#!/usr/bin/env stack
+-- stack script --resolver lts-8.17 --package foundation
+
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+import Foundation
+
+main :: IO ()
+main = putStrLn "Hello, Foundation!"
